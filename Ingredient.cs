19bed7edab7e41ffdc4b12fc1ae930c0a4001dc7
professorpanic <doctormondo@gmail.com>
2014-11-21 01:32:02 -0500

    First commit

---
+++ Ingredient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChompoDex
+{
+    [Serializable]
+    public class Ingredient
+    {
+        //all are these of strings, since users have a lot of ways to enter in info. Kept them as seperate variables for later possibly putting into a db file.
+        public string amount {get; private set;}
+        public string unitOfMeasure { get; private set; }
+        public string item { get; private set; }
+
+        public Ingredient(string amt, string measure, string thing)
+        {
+            amount = amt;
+            unitOfMeasure = measure;
+            item = thing;
+        }
+
+        
+        public override string ToString()
+        {
+
+            return amount + " " + unitOfMeasure + " " + item;
+        }
+
+    }
+}
---
+++ RecipeBook.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace ChompoDex
+{
+    [Serializable]
+    public class RecipeBook
+    {
+        public BindingList<Recipe> recipeList = new BindingList<Recipe>();
+        
+
+        public RecipeBook()
+        {
+            //configuring the BindingList to allow for manipulation
+            recipeList.AllowEdit = true;
+            recipeList.AllowNew = true;
+            recipeList.AllowRemove = true;
+            recipeList.RaiseListChangedEvents = true;
+            
+        }
+
+        
+        public void AddRecipe(Recipe recipe)
+        {
+            recipeList.Add(recipe);
+            
+        }
+        
+        public void DeleteRecipe(int itemToDelete)
+        {
+            recipeList.RemoveAt(itemToDelete);
+        }
+
+        //two methods for sorting recipes by name. They're BindingLists originally so I'm just taking the quick way of copying them into temporary list, sorting, then putting them back in.
+        public void SortRecipesAscending()
+        {
+            List<Recipe> tempList = recipeList.OrderBy(a => a.RecipeName).ToList(); ;
+            recipeList.Clear();
+            foreach (Recipe recipe in tempList)
+            {
+                recipeList.Add(recipe);
+            }
+
+        }
+
+        public void SortRecipesDescending()
+        {
+            List<Recipe> tempList = recipeList.OrderByDescending(d => d.RecipeName).ToList(); ;
+            recipeList.Clear();
+            foreach (Recipe recipe in tempList)
+            {
+                recipeList.Add(recipe);
+            }
+        }
+
+        
+
+    }
+}
